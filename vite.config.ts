d5c2a9bdd3936ebfba1c133b55330e38912e4895
JWIA182 <JWiaderek182@gmail.com>
2026-04-13 22:49:57 +0100

    fix: set base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const BASE_PATH = '/how-you-feeling/'
+
 export default defineConfig({
+  base: BASE_PATH,
   plugins: [
     vue(),
     vueJsx(),
@@ -22,21 +25,21 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: BASE_PATH,
+        start_url: BASE_PATH,
         icons: [
           {
-            src: '/icon-192.png',
+            src: `${BASE_PATH}icon-192.png`,
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: `${BASE_PATH}icon-512.png`,
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: `${BASE_PATH}icon-512.png`,
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
