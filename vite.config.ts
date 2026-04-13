1fe3b4615b37761e412c166ab7744849ec4768f1
JWIA182 <JWiaderek182@gmail.com>
2026-04-13 22:57:32 +0100

    fix: pwa manifest path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -49,6 +49,7 @@ export default defineConfig({
       injectManifest: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
       },
+      base: BASE_PATH,
     }),
   ],
   resolve: {
